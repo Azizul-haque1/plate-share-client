f687ce527dcaa4f19b7a55e155712a91a175492b
Azizul Haque <online.edu42@gmail.com>
2025-11-08 21:54:55 +0600

    project setup and some folder  structure

---
+++ src/pages/AvailableFoods/AvailableFoods.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AvailableFoods = () => {
+    return (
+        <div>
+            AvailableFoods
+
+        </div>
+    );
+};
+
+export default AvailableFoods;
